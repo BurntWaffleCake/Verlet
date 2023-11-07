03101795ecccd4ddb951e7cba86478196c238105
BurntWaffleCake <neondenetro@gmail.com>
2023-11-07 11:07:00 -0600

    cube

--- Collections.js
+++ Collections.js
@@ -1,9 +1,14 @@
 export class Node {
-  constructor(radius = 10, x, y, dx, dy) {
+  static gravity = 9.8;
+
+  constructor(radius = 10, x, y, dx = 0, dy = 0, mass = 1) {
     this.radius = radius;
+    this.pinned = false;
     this.pos = { x: x, y: y };
     this.posPrev = { x: x - dx, y: y - dy };
     this.a = { x: dx, y: dy };
+    this.mass = mass;
+    this.groundFriction = 0.7;
   }
 
   render(ctx) {
@@ -13,23 +18,104 @@ export class Node {
     ctx.stroke();
   }
 
-  tick(dt) {
-    console.log(this.pos, this.posPrev);
+  constrain(width, height) {
+    if (this.pos.x < this.radius) {
+      this.pos.x = this.radius;
+    }
+
+    if (this.pos.y < this.radius) {
+      this.pos.y = this.radius;
+    }
+
+    if (this.pos.x > width - this.radius) {
+      this.pos.x = width - this.radius;
+    }
+
+    if (this.pos.y > height - this.radius) {
+      this.pos.y = height - this.radius;
+    }
+  }
+
+  tick(dt, ctx) {
+    if (this.pinned) {
+      return;
+    }
     let Velx = this.pos.x - this.posPrev.x; // + this.a.x * dt ** 2;
     let Vely = this.pos.y - this.posPrev.y; // + this.a.y * dt ** 2;
+
+    if (this.pos.y >= ctx.canvas.height - this.radius && Velx ** 2 + Vely ** 2 > 0.000001) {
+      var m = Math.sqrt(Velx ** 2 + Vely ** 2);
+      Velx /= m;
+      Velx *= m * this.groundFriction;
+      Vely /= m;
+      Vely *= m * this.groundFriction;
+    }
 
     // this.pos.x += this.pos.x - this.posPrev.x;
     // this.pos.y += this.pos.y - this.posPrev.y;
 
     this.posPrev.x = this.pos.x;
     this.posPrev.y = this.pos.y;
+
     this.pos.x += Velx;
     this.pos.y += Vely;
+
+    this.pos.y += Node.gravity * dt;
   }
 }
 
-class Link {
-  constructor(nodeA, nodeB) {}
+export class Link {
+  constructor(nodeA, nodeB, length) {
+    this.a = nodeA;
+    this.b = nodeB;
+    this.stiffness = 1.5;
+    if (!length || typeof length != "number") {
+      this.length = Math.sqrt((this.a.pos.x - this.b.pos.x) ** 2 + (this.a.pos.y - this.b.pos.y) ** 2);
+    } else {
+      this.length = length;
+    }
+  }
+
+  tick(dt) {
+    let dx = this.b.pos.x - this.a.pos.x;
+    let dy = this.b.pos.y - this.a.pos.y;
+    let dist = Math.sqrt(dx * dx + dy * dy);
+
+    if (Math.abs(dist) < 0.001) {
+      dist = 0.001;
+      this.a.pos.x += 0.001;
+      this.b.pos.x -= 0.001;
+    }
+
+    let diff = ((this.length - dist) / dist) * this.stiffness;
+
+    let offsetx = dx * diff * 0.5;
+    let offsety = dy * diff * 0.5;
+
+    // calculate mass
+    let m1 = this.a.mass + this.b.mass;
+    let m2 = this.a.mass / m1;
+    m1 = this.b.mass / m1;
+
+    // and finally apply the offset with calculated mass
+    if (!this.a.pinned) {
+      this.a.pos.x -= offsetx * m1;
+      this.a.pos.y -= offsety * m1;
+    }
+    if (!this.b.pinned) {
+      this.b.pos.x += offsetx * m2;
+      this.b.pos.y += offsety * m2;
+    }
+  }
+
+  render(ctx) {
+    ctx.strokeStyle = "rgb(0,0,0)";
+
+    ctx.beginPath();
+    ctx.moveTo(this.a.pos.x, this.a.pos.y);
+    ctx.lineTo(this.b.pos.x, this.b.pos.y);
+    ctx.stroke();
+  }
 }
 
 export class Collection {
--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Node } from "./Collections.js";
+import { Link, Node } from "./Collections.js";
 
 const canvas = document.getElementById("source");
 const ctx = canvas.getContext("2d");
@@ -14,9 +14,32 @@ function loop(time) {
   // time in milliseconds
   let dt = time / 1000 - t;
   t = time / 1000;
-
   updateCanvasSize();
+
+  if (mousedown) {
+    let minNode = null;
+    let minDis = Number.MAX_VALUE;
+    let mindx = 0;
+    let mindy = 0;
+    for (let node of nodes) {
+      let dx = mousepos.x - node.pos.x;
+      let dy = mousepos.y - node.pos.y;
+      let dis = Math.sqrt(dx * dx + dy * dy);
+      console.log(dis);
+      if (minDis > dis) {
+        minNode = node;
+        minDis = dis;
+        mindx = dx;
+        mindy = dy;
+      }
+    }
+
+    minNode.pos.x += mindx * dt;
+    minNode.pos.y += mindy * dt;
+  }
+  // for (let dti = dt / 6; dti < dt; dti += dt / 6) {
   calculate(dt);
+  // }
   render(dt);
 
   window.requestAnimationFrame(loop);
@@ -28,14 +51,42 @@ function init() {
 
 function render(dt) {
   let size = 50;
-  node.render(ctx);
+  for (let node of nodes) {
+    node.render(ctx);
+  }
+
+  for (let link of links) {
+    link.render(ctx);
+  }
 }
 
-let node = new Node(10, ctx.canvas.width / 2, ctx.canvas.height / 2, 1, 0);
-console.log(node);
+let node = new Node(5, ctx.canvas.width / 2 - 50, ctx.canvas.height / 2 - 50, 0, 0);
+let node2 = new Node(5, ctx.canvas.width / 2 + 50, ctx.canvas.height / 2 - 50, 0, 0);
+let node3 = new Node(5, ctx.canvas.width / 2 + 50, ctx.canvas.height / 2 + 50, 0, 0);
+let node4 = new Node(5, ctx.canvas.width / 2 - 50, ctx.canvas.height / 2 + 50, 0, 0);
+// node3.pinned = true;
+
+let nodes = [node, node2, node3, node4];
+
+let link = new Link(node, node2, 100);
+let link2 = new Link(node2, node3, 100);
+let link3 = new Link(node3, node4, 100);
+let link4 = new Link(node4, node, 100);
+
+let link5 = new Link(node, node3, 141.42135623);
+let link6 = new Link(node2, node4, 141.42135623);
+
+let links = [link, link2, link3, link4, link5, link6];
 
 function calculate(dt) {
-  node.tick(dt);
+  for (let node of nodes) {
+    node.tick(dt, ctx);
+    node.constrain(ctx.canvas.width, ctx.canvas.height);
+  }
+
+  for (let link of links) {
+    link.tick(dt, ctx);
+  }
 }
 
 init();
@@ -51,3 +102,18 @@ optionsButton.onclick = function (event) {
     optionsBody.style.display = "none";
   }
 };
+
+let mousepos = { x: ctx.canvas.width / 2, y: ctx.canvas.height / 2 };
+let mousedown = false;
+document.addEventListener("mousemove", (event) => {
+  mousepos.x = event.clientX;
+  mousepos.y = event.clientY;
+});
+
+document.addEventListener("mousedown", (event) => {
+  mousedown = true;
+});
+
+document.addEventListener("mouseup", (event) => {
+  mousedown = false;
+});
